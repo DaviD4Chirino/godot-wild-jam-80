c0a35ecd2fdcd0a44cf028885fce65145221af23
David Space <genarodavid@gmail.com>
2025-04-13 19:40:39 -0400

    added spin

--- scripts/slot_machine_column.gd
+++ scripts/slot_machine_column.gd
@@ -6,5 +6,32 @@ extends Node2D
 ## The position of the column, base 0
 @export var column_id: int = -1
 
+var selected_token: Token
+
+
+@export var spinning: bool = false: set = set_spinning
+
 func get_size() -> Vector2:
 	return %Sprite.texture.get_size()
+
+func start_spinning() -> void:
+	spinning = true
+
+func finish_spinning() -> void:
+	spinning = false
+
+func spin() -> void:
+	for slot_node: Slot in %Slots.get_children():
+		slot_node.data = tokens.pick_random()
+
+
+func set_spinning(val: bool) -> void:
+	spinning = val
+	if spinning:
+		spin()
+		%SpinTimer.start()
+	if !spinning:
+		%SpinTimer.stop()
+
+func _on_spin_timer_timeout() -> void:
+	spin()

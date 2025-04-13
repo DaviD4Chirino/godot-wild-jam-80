7bd202bdcb8a3b406bf6328e3b48a8b3219b1cdd
David Space <genarodavid@gmail.com>
2025-04-13 15:30:39 -0400

    made SlotData Resource

--- scripts/slot_machine.gd
+++ scripts/slot_machine.gd
@@ -1,7 +1,7 @@
 @tool
 extends SlotMachine
 
-@export var column_sprite: Texture2D
+@export var column_scene: PackedScene
 
 @export_range(1, 5, 1) var columns: int = 3: set = set_columns;
 
@@ -13,8 +13,8 @@ func _ready():
 	generate_columns(columns)
 
 func set_columns(val: int) -> void:
-	if !column_sprite:
-		printerr("column_sprite is empty")
+	if !column_scene:
+		printerr("column_scene is empty")
 		return
 
 	columns = val
@@ -22,23 +22,16 @@ func set_columns(val: int) -> void:
 	generate_columns(columns)
 	
 
-func create_column_sprite() -> Sprite2D:
-	var new_sprite: Sprite2D = Sprite2D.new()
-	new_sprite.texture = column_sprite
-	new_sprite.centered = false
-	return new_sprite
-
-
 func generate_columns(amt: int) -> void:
 	for child: Node2D in %Columns.get_children():
 		child.queue_free()
 	
 	for i in amt:
-		var column: Sprite2D = create_column_sprite()
-		var size_x: float = (column_sprite.get_size().x - 2) * i
+		var column: Node2D = column_scene.instantiate()
+		var size_x: float = (column.get_size().x - 2) * i
 		
 		column.position.x += size_x
-		%Lever.position.x = (size_x + (column_sprite.get_size().x))
+		%Lever.position.x = (size_x + (column.get_size().x))
 		%Columns.add_child(column)
 
 

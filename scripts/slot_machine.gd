5884d459e6ebd7e3a0582361732bc5f6c629c1d7
David Space <genarodavid@gmail.com>
2025-04-13 23:01:08 -0400

    added roll specific column

--- scripts/slot_machine.gd
+++ scripts/slot_machine.gd
@@ -1,3 +1,4 @@
+@tool
 extends SlotMachine
 @export var column_scene: PackedScene
 @export_group("Nodes")
@@ -28,12 +29,29 @@ func _input(event: InputEvent):
 					child.start_spinning()
 
 			await get_tree().create_timer(roll_duration).timeout
+			
+
+			for i: int in columns_node.get_children().size():
+				stop_spinning_column(i)
+				await get_tree().create_timer(0.3).timeout
 
 			lever_node.play("default")
-			if columns_node:
-				for child: Node2D in columns_node.get_children():
-					child.stop_spinning()
+			
 
+func start_spinning_column(column_id: int) -> void:
+	get_column(column_id).start_spinning()
+
+func stop_spinning_column(column_id: int) -> void:
+	get_column(column_id).stop_spinning()
+
+func get_column(column_id: int) -> Node:
+	assert(columns_node, "columns_node is null")
+
+	var _columns: Array[Node] = columns_node.get_children()
+
+	assert(_columns.size() >= 0, "columns_node does not have children")
+	
+	return _columns[clampi(column_id, 0, _columns.size())]
 
 func set_columns(val: int) -> void:
 	if !column_scene:
@@ -61,10 +79,10 @@ func generate_columns(amt: int) -> void:
 		var size_x: float = (column.get_size().x - 2) * i
 
 		if inventory.size() >= i:
-			print("true")
 			column.tokens = inventory[i - 1].tokens
 
 		column.position.x += size_x
+		column.column_id = i
 		lever_node.position.x = (size_x + (column.get_size().x))
 		columns_node.add_child(column)
 

46fd98e1478be08d0ad70cfa89617edc641896c8
David Space <genarodavid@gmail.com>
2025-04-15 17:09:49 -0400

    added swapable shaders

--- scripts/characters/enemy/enemy_sprite.gd
+++ scripts/characters/enemy/enemy_sprite.gd
@@ -1,7 +1,21 @@
 @tool
 extends Sprite2D
-@export var trigger_shock: bool = false: set = set_trigger_shock
 
+enum Shaders {
+	SHOCK_DAMAGE,
+	FLASH,
+}
+
+@export var current_shader: Shaders = Shaders.FLASH: set = set_current_shader
+
+@export var activate_shader: bool = false: set = set_activate_shader
+
+var trigger_shock: bool = false
+var trigger_flash: bool = false: set = set_trigger_flash
+
+@export_group("shaders", "shader_")
+@export var shader_flash: Shader
+@export var shader_shock: Shader
 
 var shock_time: float = 0.0
 
@@ -13,6 +27,48 @@ func _physics_process(delta: float) -> void:
 		shock_time += delta
 		(material as ShaderMaterial).set_shader_parameter("shock_time", shock_time)
 
+func swap_shaders() -> void:
+	match current_shader:
+		Shaders.FLASH:
+			(self.material as ShaderMaterial).shader = shader_flash
+
+		Shaders.SHOCK_DAMAGE:
+			(self.material as ShaderMaterial).shader = shader_shock
+		_:
+			(self.material as ShaderMaterial).shader = null
+
+			
+func set_current_shader(val: Shaders) -> void:
+	current_shader = val
+	activate_shader = false
+	swap_shaders()
+
+func set_activate_shader(val: bool) -> void:
+	activate_shader = val
+	match current_shader:
+		Shaders.FLASH:
+			use_flash()
+		Shaders.SHOCK_DAMAGE:
+			use_shock()
+		
+
+func use_flash() -> void:
+	(material as ShaderMaterial).set_shader_parameter("active", activate_shader)
+
+func use_shock() -> void:
+	trigger_shock = activate_shader
+
+	if !trigger_shock:
+		shock_time = 0.0
+		(material as ShaderMaterial).set_shader_parameter("shock_time", shock_time)
+
+	(material as ShaderMaterial).set_shader_parameter("trigger_shock", trigger_shock)
+
+
+func set_trigger_flash(val: bool) -> void:
+	trigger_flash = val
+	if trigger_flash:
+		(material as ShaderMaterial).set_shader_parameter("active", true)
 
 func set_trigger_shock(val: bool) -> void:
 	trigger_shock = val

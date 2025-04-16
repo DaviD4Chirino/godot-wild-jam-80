e7eae803a1132f53186651eec730571ef21c6716
David Space <genarodavid@gmail.com>
2025-04-15 23:12:25 -0400

    added timers and made things a bit prettier

---
+++ components/PlayAnimation.gd
@@ -0,0 +1,20 @@
+extends Component
+## Something that plays animations purely by signals
+class_name PlayAnimationComponent
+
+
+@export var animation_player: AnimationPlayer
+@export var animation_to_play: StringName = &""
+
+@export_group("Configuration")
+
+func _enter_tree() -> void:
+	assert(animation_player, "animation_player is not set")
+	assert(
+		animation_player.has_animation(animation_to_play),
+		"animation_player do not have %s" % animation_to_play
+	)
+
+func play_animation() -> void:
+	if !enabled: return
+	animation_player.play(animation_to_play)

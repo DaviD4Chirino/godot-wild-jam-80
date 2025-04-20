96ee414e452798cadfc7fe63dec2648f0207389e
David Space <genarodavid@gmail.com>
2025-04-19 20:42:02 -0400

    added cutoff shader

--- scripts/characters/enemy/enemy_sprite.gd
+++ scripts/characters/enemy/enemy_sprite.gd
@@ -4,11 +4,13 @@ extends Sprite2D
 enum Shaders {
 	SHOCK_DAMAGE,
 	FLASH,
+	CUT,
 }
 
 @export var current_shader: Shaders = Shaders.FLASH: set = set_current_shader
 
 @export var activate_shader: bool = false: set = set_activate_shader
+@export_range(0.0, 1.0, 0.01) var shader_cut_percentage: float = 1.0: set = set_shader_cut_percentage
 
 var trigger_shock: bool = false
 var trigger_flash: bool = false: set = set_trigger_flash
@@ -16,6 +18,7 @@ var trigger_flash: bool = false: set = set_trigger_flash
 @export_group("shaders", "shader_")
 @export var shader_flash: Shader
 @export var shader_shock: Shader
+@export var shader_cut: Shader
 
 var shock_time: float = 0.0
 
@@ -31,9 +34,10 @@ func swap_shaders() -> void:
 	match current_shader:
 		Shaders.FLASH:
 			(self.material as ShaderMaterial).shader = shader_flash
-
 		Shaders.SHOCK_DAMAGE:
 			(self.material as ShaderMaterial).shader = shader_shock
+		Shaders.CUT:
+			(self.material as ShaderMaterial).shader = shader_cut
 		_:
 			(self.material as ShaderMaterial).shader = null
 
@@ -50,7 +54,18 @@ func set_activate_shader(val: bool) -> void:
 			use_flash()
 		Shaders.SHOCK_DAMAGE:
 			use_shock()
-		
+		# Shaders.CUT:
+		# 	use_cut()
+
+func set_shader_cut_percentage(val: float) -> void:
+	shader_cut_percentage = val
+
+	if current_shader != Shaders.CUT: return
+
+	(material as ShaderMaterial).set_shader_parameter("cutoff", shader_cut_percentage)
+
+func use_cut() -> void:
+	pass
 
 func use_flash() -> void:
 	(material as ShaderMaterial).set_shader_parameter("active", activate_shader)
